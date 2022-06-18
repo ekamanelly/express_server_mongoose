f7d0a78b352c2f851e8116f18017d3db10663d9a
ekamanelly <ekamanelly@gmail.com>
2022-06-18 23:52:39 +0100

    _

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { createApp } from "./app";
 import { db } from "./database";
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = createApp();
 const PORT = 9000;
 db(
-  "mongodb+srv://jodegauto:R6bNsmEF84AdiLRh@vorem.zly4i.mongodb.net/expressApp?retryWrites=true&w=majority"
+  "mongodb+srv://your_mongodb_string"
 ).then(() => {
   app.listen(PORT, async () => {
     console.log("server connected to port:", PORT);
