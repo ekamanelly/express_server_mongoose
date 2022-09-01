00187593aa2e4b16f6bb165c39d9e2e0f3e056fb
ekamanelly <ekamanelly@gmail.com>
2022-09-01 14:29:03 +0100

    added coverage threshold

--- app/jest.config.js
+++ app/jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 99,
+    },
+  },
 };
